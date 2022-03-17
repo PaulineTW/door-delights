c96a9cd183619cc4f372b5ff0d77ac4116c9a8aa
pauline rouger <paulinerouge@hotmail.com>
2022-03-17 18:08:08 +0800

    Implementing Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -11,8 +11,32 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open")
 })
 
+// Smooth scrolling animation (in some Safari versions)
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    console.log(href);
 
-///////////////////////////////////////////////////////////
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    // Scroll to other link section
+    if (href !== "#" && href.startsWith("#")){
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: "smooth"});
+    }
+    // Close mobile nav
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  })
+})
+
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -31,8 +55,6 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
